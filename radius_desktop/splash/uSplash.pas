91b7d76121c7d58cf162c08f1fb7fe7dbb1d16b6
jonathansmorais <jonathansmorais@gmail.com>
2016-02-16 23:11:14 -0200

    Acerto

--- radius_desktop/splash/uSplash.pas
+++ radius_desktop/splash/uSplash.pas
@@ -83,7 +83,7 @@ procedure TfrmSplash.bbVerificarLoginClick(Sender: TObject);
 var
   serialCliente: string;
 begin
-  setDataVerdadeira(); //Faz tratamento inicial para segurança da data real
+  setDataVerdadeira(); //Faz tratamento inicial para segurança de data real.
 
   serialCliente := buscarSerialCliente();
 
